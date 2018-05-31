28bb891b1fd619d9bc35eed201ebe6147e724be9
Sidderman <s.i.sidorovkrd@yandex.ru>
2018-05-31 10:14:34 +0300

    Частично разметка

--- src/index.php
+++ src/index.php
@@ -1,12 +1,237 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
 </head>
 <body>
+
+	<header class="page-header">
+		<div class="container">
+			<div class="page-header__box">
+				<a href="/" class="logo">
+					<p class="logo__title">autocartex.ru</p>
+					<p class="logo__text">Продажа беспроводных акустических систем по всей России</p>
+				</a>
+
+				<div class="header-phone">
+					<a href="#" class="header-phone__item">8(800)500-62-57</a>
+					<a href="#" class="header-phone__text">Заказать звонок</a>
+				</div>
+			</div>
+			<div class="page-header__box">
+				<ul class="main-menu">
+					<li class="main-menu__item"><a href="#" class="main-menu__link">Главная</a></li>
+					<li class="main-menu__item"><a href="#" class="main-menu__link">Каталог</a></li>
+					<li class="main-menu__item"><a href="#" class="main-menu__link">Зачем нужны</a></li>
+					<li class="main-menu__item"><a href="#" class="main-menu__link">Преимущества</a></li>
+					<li class="main-menu__item"><a href="#" class="main-menu__link">Отзывы</a></li>
+					<li class="main-menu__item"><a href="#" class="main-menu__link">Доставка</a></li>
+				</ul>
+			</div>
+		</div>
+	</header>
+
+	<section class="promo">
+		<div class="container">
+			<div class="promo__box">
+				<p class="promo__title">Беспроводные колонки от 1 190 руб!</p>
+				<div class="promo__img-wrapper">
+					<img src="" alt="" class="promo__img">
+				</div>
+				<ul class="promo__list">
+					<li class="promo__item">Можно брать куда угодно - любимая музыка всегда будет с Вами!</li>
+					<li class="promo__item">Станет отличным подарком по любому поводу.</li>
+					<li class="promo__item">Приятная цена - доступно каждому!</li>
+				</ul>
+			</div>
+			<div class="gift-offer">
+				<p class="gift-offer__title">Powerbank в подарок!</p>
+				<p class="gift-offer__text">При покупке динамика <span>JBL CHARGE 3</span></p>
+				<div class="gift-offer__img-wrapper">
+					<img src="" alt="" class="gift-offer__img">
+				</div>
+				<p class="gift-offer__text"><span>+ПОДАРОК!</span> Зарядное устройство <span>Remax Mini</span></p>
+
+				<form action="" class="gift-offer__form">
+					<input type="text" class="gift-offer__input" placeholder="Ваше имя">
+					<input type="text" class="gift-offer__input" placeholder="Ваш номер телефона">
+					<input type="submit" class="gift-offer__submit" value="Купить JBL Cherge 3">
+				</form>
+			</div>
+		</div>
+	</section>
+
+	<section class="advantages">
+		<div class="container">
+			<h2 class="advantages__title">Портативные колонки - музыка всегда с тобой!</h2>
+
+			<div class="advantages__wrapper">
+				<div class="advantages__item">
+					<div class="advantages__img-wrapper">
+						<img src="" alt="" class="advantages__img">
+					</div>
+					<p class="advantages__text">Оригинальные колонки Autocartex.ru</p>
+				</div>
+				<div class="advantages__item">
+					<div class="advantages__img-wrapper">
+						<img src="" alt="" class="advantages__img">
+					</div>
+					<p class="advantages__text">Более 4 лет на рынке</p>
+				</div>
+				<div class="advantages__item">
+					<div class="advantages__img-wrapper">
+						<img src="" alt="" class="advantages__img">
+					</div>
+					<p class="advantages__text">Официальная гарантия 365 дней</p>
+				</div>
+				<div class="advantages__item">
+					<div class="advantages__img-wrapper">
+						<img src="" alt="" class="advantages__img">
+					</div>
+					<p class="advantages__text">Бесплатная доставка по всей России</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="catalog">
+		<div class="container">
+			<h2 class="catalog__title">Выберите идеальную колонку Autocartex!</h2>
+			<div class="catalog__wrapper">
+
+				<div class="card-item">
+					<div class="card-item__gallery">
+						
+					</div>
+					<div class="card-item__info">
+						<p class="card-item__title"></p>
+						<div class="card-item__rating"></div>
+						<p class="card-item__short-description"></p>
+						<p class="card-item__description"></p>
+						<ul class="card-item__list">
+							<li class="card-item__item"></li>
+							<li class="card-item__item"></li>
+							<li class="card-item__item"></li>
+							<li class="card-item__item"></li>
+							<li class="card-item__item"></li>
+							<li class="card-item__item"></li>
+						</ul>
+						<div class="card-item__gift-img-wrapper">
+							<img src="" alt="" class="card-item__gift-img">
+						</div>
+						<p class="card-item__price"></p>
+						<a href="" class="card-item__buy-btn"></a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<section class="promo promo--bottom">
+		<div class="container">
+			<p class="promo__title"></p>
+
+			<div class="promo__video-wrapper">
+				
+			</div>
+
+			<div class="promo__info-icon">
+				<div class="info-icon">
+					<div class="info-icon__img-wrapper">
+						<img src="" alt="" class="info-icon__img">
+					</div>
+					<p class="info-icon__text">Для смартфона</p>
+				</div>
+				<div class="info-icon">
+					<div class="info-icon__img-wrapper">
+						<img src="" alt="" class="info-icon__img">
+					</div>
+					<p class="info-icon__text">Для вечеринок на природе</p>
+				</div>
+				<div class="info-icon">
+					<div class="info-icon__img-wrapper">
+						<img src="" alt="" class="info-icon__img">
+					</div>
+					<p class="info-icon__text">Для активного отдыха</p>
+				</div>
+				<div class="info-icon">
+					<div class="info-icon__img-wrapper">
+						<img src="" alt="" class="info-icon__img">
+					</div>
+					<p class="info-icon__text">Для путешествий</p>
+				</div>
+
+				<div class="info-icon__btn">Выбрать колонку</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="infographic">
+		<div class="container">
+			<h2 class="advantages__title">Преимущества беспроводных колонок</h2>
+
+			<div class="infographic__item">
+				<p class="infographic__title">Берите с собой куда угодно</p>
+				<img src="" alt="" class="infographic__img">
+			</div>
+			<div class="infographic__item">
+				<p class="infographic__title">Музыка на любой вечеринке</p>
+				<img src="" alt="" class="infographic__img">
+			</div>
+			<div class="infographic__item">
+				<p class="infographic__title">Хороший подарок <br>по любому поводу</p>
+				<img src="" alt="" class="infographic__img">
+			</div>
+			<div class="infographic__item">
+				<p class="infographic__title">Недорого - доступно <br>каждому</p>
+				<img src="" alt="" class="infographic__img">
+			</div>
+			<div class="infographic__item">
+				<p class="infographic__title">До 12 ч звучания и <br>влагозащита</p>
+				<img src="" alt="" class="infographic__img">
+			</div>
+			
+		</div>
+	</section>
+
+
+	<section class="review__wrapper">
+		<div class="container">
+
+			<h2 class="promo__title">Отзывы наших покупателей</h2>
+
+			<div class="review__card-wrapper">
+				<div class="review">
+					<div class="review__img-wrapper">
+						<img src="" alt="" class="review__img">
+					</div>
+
+					<div class="review__info">
+						<p class="review__name"></p>
+						<p class="review__text"></p>
+						<p class="review__text"></p>
+						<div class="review__rating"></div>
+					</div>
+
+					<div class="review__bq">
+						<p class="review__title"></p>
+						<p class="review__bq-text"></p>
+					</div>
+
+				</div>
+			</div>			
+		</div>
+	</section>
+
+	<section class="delivery">
+		<div class="container"></div>
+	</section>
+
+
 
 </body>
 </html>
